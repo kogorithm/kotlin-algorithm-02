c3fd56ef7b527b8be0bee34711c694d61e12b235
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2022-09-26 02:27:11 +0900

    [Setting]

---
+++ src/1week/jaewon/test.kt
@@ -0,0 +1,21 @@
+
+class `Q1` {
+    companion object {
+        fun getSolution(): Solution {
+            return Solution()
+        }
+    }
+
+    class Solution {
+        fun solution(): Int {
+           var answer = 0
+
+
+            return answer
+        }
+    }
+}
+
+fun main() {
+    println(Q1.getSolution().solution())
+}
