0178ed267a646d1520c39382b1dfe54577746ded
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2022-09-28 20:33:19 +0900

    [Update]

--- src/1week/jaewon/현수막.kt
+++ src/1week/jaewon/현수막.kt
@@ -2,66 +2,51 @@
  *  사용할 알고리즘 : BFS
  *  변수
  *  - graph : 현수막 정보 <MutableList<List<Int>>
- *  - visited : 방문 정보 <MutableList<List<Bolean>>
  *  - count : 반환할 글자 수 <answer>
  *  함수
- *  - bfs(시작인덱스 <Pair()> , visited , graph ) : 방문 정보 반환
+ *  - bfs(시작인덱스 <Pair()>)
  *
- *  1. graph에서 1인 부분 visited에서 전부 false 로 변경
- *  2. visited가 전부 true일 때까지 반복
- *  3. visited에서 true인 인덱스 bfs에 전달하며 반복
- *  4. 함수를 호출한 횟수 = 글자 수
+ *  1. graph가 전부 0일 때까지 반복
+ *  2. graph가 1인 좌표를 bfs에 전달하여 순회
+ *  3. 함수를 호출한 횟수 = 글자 수
  */
 
 
-val graph : MutableList<List<Int>> = mutableListOf()
+val `1weekjaewon01graph` : MutableList<MutableList<Int>> = mutableListOf()
+// 전역변수여서 변수명 겹칠까봐 안겹치도록 길게 만들어서 커밋찍었습니다..! 백준 코드 제출은 다르게 했어요!!
 
 fun main(){
     val (n,m) = readln().split(" ").map { it.toInt() }
-    var visited = MutableList(n){ MutableList(m){true} }
     var count = 0
-    repeat(n){ x->
-        val list = readln().split(" ").map { it.toInt() }
-        list.forEachIndexed { y , it ->
-            if(it == 1){ visited[x][y] = false}
-        }
-        graph.add(list)
+    repeat(n){
+        val list = readln().split(" ").map { it.toInt() }.toMutableList()
+        `1weekjaewon01graph`.add(list)
     }
 
-
-
-    while (true){
-        if (visited.count { it.all { it} } == n){
-            break
-        }
-
-        var x = 0
-        var y = 0
-        run loop@{
-            visited.forEachIndexed { xx, it ->
-                y = it.indexOf(false)
-                if (y != -1){
-                    x = xx
+    run loop@{
+        `1weekjaewon01graph`.forEachIndexed { x, list ->
+            list.forEachIndexed { y, i ->
+                if (`1weekjaewon01graph`.count {it.all { it == 0} } == n){
                     return@loop
+                }
+                if(`1weekjaewon01graph`[x][y] == 1){
+                    bfs(Pair(x,y))
+                    count++
                 }
             }
         }
-
-        visited = bfs(Pair(x,y),visited)
-        count++
     }
 
     println(count)
 }
 
-fun bfs(start : Pair<Int,Int>,visited : MutableList<MutableList<Boolean>>)
-: MutableList<MutableList<Boolean>> {
+fun bfs(start : Pair<Int,Int>) {
     val queue = mutableListOf<Pair<Int, Int>>()
     val dx = intArrayOf(-1, 0, 1, 0, -1, -1, 1, 1)
     val dy = intArrayOf(0, -1, 0, 1, -1, 1, -1, 1)
 
     queue.add(start) // 초기 정점 큐에 추가
-    visited[start.first][start.second] = true
+    `1weekjaewon01graph`[start.first][start.second] = 0
 
     while (queue.isNotEmpty()) {
         repeat(queue.size) {
@@ -71,15 +56,13 @@ fun bfs(start : Pair<Int,Int>,visited : MutableList<MutableList<Boolean>>)
             repeat(8) { time ->
                 val nx = x + dx[time]
                 val ny = y + dy[time]
-                if (nx in graph.indices && ny in 0 until graph[0].size && graph[nx][ny] == 1) {
-                    if (!visited[nx][ny]) {
-                        visited[nx][ny] = true
+                if (nx in `1weekjaewon01graph`.indices && ny in 0 until `1weekjaewon01graph`[0].size && `1weekjaewon01graph`[nx][ny] == 1) {
+                    if (`1weekjaewon01graph`[nx][ny] != 0) {
+                        `1weekjaewon01graph`[nx][ny] = 0
                         queue.add(Pair(nx, ny))
                     }
                 }
             }
         }
     }
-
-    return visited
 }
