25b9976ad989f38f0af41f27216725ce3fae7d88
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2022-10-03 02:36:03 +0900

    두번째 코드

--- src/1week/jaewon/스타트와 링크.kt
+++ src/1week/jaewon/스타트와 링크.kt
@@ -3,29 +3,46 @@ import kotlin.math.min
 
 
 val ability : MutableList<List<Int>> = mutableListOf()
+var min = 1000
 
 fun main(){
-    var min = 1000
     val n = readln().toInt()
     val temp = MutableList(n){0}
     val answer = mutableListOf<List<Int>>()
+    var visited = Array<Boolean>(n, {false});
     repeat(n){x ->
         temp[x] = x+1
         val list = readln().split(" ").map { it.toInt() }
         ability.add(list)
     }
 
-    combination(answer,temp,Array<Boolean>(n){false},0,n/2)
-    var i = 0
-    var j = answer.lastIndex
-    while (true){
-        if (min == 0 || i > j ) break
-        min = min(min, abs(getAbility(answer[i])-getAbility(answer[j])))
-        i++
-        j--
+    fun dfs(index : Int, depth : Int){
+        if(depth == n/2){
+            val team1 : MutableList<Int> = mutableListOf()
+            val team2 : MutableList<Int> = mutableListOf()
+            visited.forEachIndexed { index, it ->
+                if(it) { team1.add(index+1)}
+                else { team2.add(index+1)}
+            }
+            min = min(min, abs(getAbility(team1)-getAbility(team2)))
+            if(min == 0) {
+                println(min)
+                System.exit(0);
+            }
+            return;
+        }
+        for (i in index until n){
+            if(visited[i]) continue;
+            visited[i] = true;
+            dfs(i+1, depth + 1);
+            visited[i] = false;
+        }
     }
+
+    dfs(0,0)
     println(min)
 }
+
 
 fun getAbility(list : List<Int>): Int{
     var sum = 0
@@ -33,18 +50,6 @@ fun getAbility(list : List<Int>): Int{
         sum += ability[it[0]-1][it[1]-1]
     }
     return sum
-}
-
-fun <T> combination(answer: MutableList<List<T>>, el: List<T>, ck: Array<Boolean>, start: Int, target: Int) {
-    if(target == 0) {
-        answer.addAll( listOf(el.filterIndexed { index, t -> ck[index] }) )
-    } else {
-        for(i in start until el.size) {
-            ck[i] = true
-            combination(answer, el, ck, i + 1, target - 1)
-            ck[i] = false
-        }
-    }
 }
 
 fun <T> permutation(el: List<T>, fin: List<T> = listOf(), sub: List<T> = el ): List<List<T>> {
