9e0cb6c3552ccaf40d57c54ddf50562efa5cda1d
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2022-10-10 21:04:28 +0900

    [Refact]

--- src/1week/jaewon/스타트와 링크.kt
+++ src/1week/jaewon/스타트와 링크.kt
@@ -2,58 +2,74 @@ import kotlin.math.abs
 import kotlin.math.min
 import kotlin.system.exitProcess
 
-
-val ability : MutableList<List<Int>> = mutableListOf()
-var min = 1000
-
-fun main(){
-    val n = readln().toInt()
-    val temp = MutableList(n){0}
-    val answer = mutableListOf<List<Int>>()
-    var visited = Array<Boolean>(n, {false});
-    repeat(n){x ->
-        temp[x] = x+1
-        val list = readln().split(" ").map { it.toInt() }
-        ability.add(list)
+class `스타트와 링크` {
+    companion object {
+        fun getSolution(): Solution {
+            return Solution()
+        }
     }
 
-    fun getAbility(){
-        var team1 = 0
-        var team2 = 0
+    class Solution {
 
-        for(i in 0 until n-1) {
-            for(j in i+1 until n) {
+        val ability : MutableList<List<Int>> = mutableListOf()
+        var min = 1000
+        fun solution(){
+            val n = readln().toInt()
+            val temp = MutableList(n){0}
+            val answer = mutableListOf<List<Int>>()
+            var visited = Array<Boolean>(n, {false});
+            repeat(n){x ->
+                temp[x] = x+1
+                val list = readln().split(" ").map { it.toInt() }
+                ability.add(list)
+            }
 
-                if(visited[i] && visited[j]) {
-                    team1 += ability[i][j] + ability[j][i];
+            fun getAbility(){
+                var team1 = 0
+                var team2 = 0
+
+                for(i in 0 until n-1) {
+                    for(j in i+1 until n) {
+
+                        if(visited[i] && visited[j]) {
+                            team1 += ability[i][j] + ability[j][i];
+                        }
+                        else if( !visited[i] && !visited[j] ) {
+                            team2 += ability[i][j] + ability[j][i];
+                        }
+                    }
                 }
-                else if( !visited[i] && !visited[j] ) {
-                    team2 += ability[i][j] + ability[j][i];
+                min = min(min, abs(team1-team2))
+                if(min == 0) {
+                    println(min)
+                    exitProcess(0)
                 }
             }
-        }
-        min = min(min, abs(team1-team2))
-        if(min == 0) {
+
+            fun dfs(index : Int, depth : Int){
+                if(depth == n/2){
+                    getAbility()
+                    return;
+                }
+                for (i in index until n){
+                    if(visited[i]) continue;
+                    visited[i] = true;
+                    dfs(i+1, depth + 1);
+                    visited[i] = false;
+                }
+            }
+
+
+            dfs(0,0)
             println(min)
-            exitProcess(0)
         }
+
     }
-
-    fun dfs(index : Int, depth : Int){
-        if(depth == n/2){
-            getAbility()
-            return;
-        }
-        for (i in index until n){
-            if(visited[i]) continue;
-            visited[i] = true;
-            dfs(i+1, depth + 1);
-            visited[i] = false;
-        }
-    }
-
-
-    dfs(0,0)
-    println(min)
 }
+
+fun main() {
+    `스타트와 링크`.getSolution().solution()
+}
+
+
 
