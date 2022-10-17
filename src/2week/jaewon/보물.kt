736b3b33bdffcb2749df4fbbecfcf26f08bbf842
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2022-10-17 18:36:42 +0900

    [Refact] 코드리뷰 반영

--- src/2week/jaewon/보물.kt
+++ src/2week/jaewon/보물.kt
@@ -2,11 +2,12 @@ package `2week`.jaewon
 
 fun main(){
     var answer = 0
-    val n = readln().toInt()
+    readln().toInt()
     val arrA = readln().split(" ").map { it.toInt() }.sorted()
     val arrB = readln().split(" ").map { it.toInt() }.sortedDescending()
 
-    answer = arrA.foldIndexed(0) { index, total , i ->  total + (i*arrB[index]) }
+    //answer = arrA.foldIndexed(0) { index, total , i ->  total + (i*arrB[index]) }
+    answer = arrA.zip(arrB).sumOf { (a,b) -> a*b }
 
     println(answer)
 }
--- src/2week/jaewon/성격 유형 검사하기.kt
+++ src/2week/jaewon/성격 유형 검사하기.kt
@@ -1,3 +1,5 @@
+import kotlin.math.abs
+
 /**
  *
  */
@@ -14,18 +16,19 @@ class `성격 유형 검사하기` {
             val type = mutableMapOf<Char,Int>('R' to 0,'T' to 0 ,'C' to 0 ,'F' to 0 ,'J' to 0 ,'M' to 0 ,'A' to 0 ,'N' to 0 )
 
             choices.zip(survey).forEach {
-                when(it.first){
-                    1 -> type[it.second[0]]  = type[it.second[0]]!! + 3
-                    2 -> type[it.second[0]]= type[it.second[0]]!! + 2
-                    3 -> type[it.second[0]]= type[it.second[0]]!! + 1
-                    in 5..7 -> type[it.second[1]]= type[it.second[1]]!! + it.first-4
+                val (choice, count) = it
+                when(choice){
+                    1,2,3 -> type[count[0]]  = type[count[0]]!! + abs(4 - choice)
+                    5,6,7 -> type[it.second[1]]= type[it.second[1]]!! + abs( choice-4)
                 }
             }
-            println(type.toString())
-            answer += if (type['R']!! >= type['T']!!){ "R" }else "T"
-            answer += if (type['C']!! >= type['F']!!){ "C" }else "F"
-            answer += if (type['J']!! >= type['M']!!){ "J" }else "M"
-            answer += if (type['A']!! >= type['N']!!){ "A" }else "N"
+            answer = listOf( Pair('R', 'T'), Pair('C', 'F'), Pair('J', 'M'), Pair('A', 'N') )
+                .joinToString { (a,b) -> (if(type[a]!! >= type[b]!!) a else b ).toString()}
+
+//            answer += if (type['R']!! >= type['T']!!){ "R" }else "T"
+//            answer += if (type['C']!! >= type['F']!!){ "C" }else "F"
+//            answer += if (type['J']!! >= type['M']!!){ "J" }else "M"
+//            answer += if (type['A']!! >= type['N']!!){ "A" }else "N"
 
             return answer
         }
