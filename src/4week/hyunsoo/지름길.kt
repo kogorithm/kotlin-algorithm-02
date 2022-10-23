c2d907c0a129c5c9a575fcf9a6e1b5bac79522e6
soopeach <hsjeon584@gmail.com>
2022-10-24 04:04:30 +0900

    Refactor: 지름길 코드 충돌 안나도록 수정

--- src/4week/hyunsoo/지름길.kt
+++ src/4week/hyunsoo/지름길.kt
@@ -18,33 +18,42 @@ import java.lang.Math.min
  *
  */
 
-data class ShortCutData(val start: Int, val distance: Int)
+class 전현수_지름길{
 
-fun main() {
+    data class ShortCutData(val start: Int, val distance: Int)
 
-    val (shortCutCnt, highWayLength) = readln().split(" ").map { it.toInt() }
-    val dp = Array(10001) { i -> i }
-    val shortCutGraph = Array(10001) {
-        mutableListOf<ShortCutData>()
-    }
+    fun solution() {
 
-    repeat(shortCutCnt) {
-        val (start, end, shortCutDistance) = readln().split(" ").map { it.toInt() }
-        // 도착지점이 고속도로의 길이를 넘거나 지름길이 도로보다 길지 않은 경우에만 지름길 생성
-        if ((end > highWayLength).not() &&
-            (end - start <= shortCutDistance).not()
-        ) shortCutGraph[end].add(ShortCutData(start, shortCutDistance))
+        val (shortCutCnt, highWayLength) = readln().split(" ").map { it.toInt() }
+        val dp = Array(10001) { i -> i }
+        val shortCutGraph = Array(10001) {
+            mutableListOf<ShortCutData>()
+        }
 
-    }
+        repeat(shortCutCnt) {
+            val (start, end, shortCutDistance) = readln().split(" ").map { it.toInt() }
+            // 도착지점이 고속도로의 길이를 넘거나 지름길이 도로보다 길지 않은 경우에만 지름길 생성
+            if ((end > highWayLength).not() &&
+                (end - start <= shortCutDistance).not()
+            ) shortCutGraph[end].add(ShortCutData(start, shortCutDistance))
 
-    for (end in 1..highWayLength) {
-        dp[end] = dp[end - 1] + 1
-        if (shortCutGraph[end].isNotEmpty()) {
-            shortCutGraph[end].forEach { shortCutData ->
-                dp[end] = min(dp[end], dp[shortCutData.start] + shortCutData.distance)
+        }
+
+        for (end in 1..highWayLength) {
+            dp[end] = dp[end - 1] + 1
+            if (shortCutGraph[end].isNotEmpty()) {
+                shortCutGraph[end].forEach { shortCutData ->
+                    dp[end] = min(dp[end], dp[shortCutData.start] + shortCutData.distance)
+                }
             }
         }
+
+        println(dp[highWayLength])
     }
 
-    println(dp[highWayLength])
+}
+
+fun main(){
+    val myClass = 전현수_지름길()
+    myClass.solution()
 }
