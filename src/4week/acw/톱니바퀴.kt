49945cc258465c9fbae1d7619a55be4e1f66e888
acw <acwoo97@gmail.com>
2022-10-26 05:17:45 +0900

    톱니바퀴  해결

--- src/4week/acw/톱니바퀴.kt
+++ src/4week/acw/톱니바퀴.kt
@@ -8,20 +8,22 @@ const val RIGHT = 5
 const val LEFT = 1
 
 class `acw톱니바퀴` {
+
     var wheel = Array(4) { 0 }
     var visit = Array(4) { false }
     var turn = arrayListOf<Pair<Int, Int>>()
 
 
     fun addTurn(w: Int, turnD: Int) {
+        //dfs로 들어가면서 회전해야할 것들 turn에 추가해주기
 
 
         var wheelNowRight = if (wheel[w] and (1 shl RIGHT) != 0) 1 else 0
         var wheelNowLeft = if (wheel[w] and (1 shl LEFT) != 0) 1 else 0
-        println("$w $wheelNowLeft $wheelNowRight")
 
-        if (w - 1 > 0) {
+        if (w - 1 >= 0) {
             val leftWheelsRight = if (wheel[w - 1] and (1 shl RIGHT) != 0) 1 else 0
+
             if (wheelNowLeft != leftWheelsRight && !visit[w - 1]) {
                 visit[w - 1] = true
                 turn.add(Pair(w - 1, turnD * -1))
@@ -30,7 +32,7 @@ class `acw톱니바퀴` {
         }
 
         if (w + 1 < 4) {
-            val rightWheelsLeft = if (wheel[w - 1] and (1 shl LEFT) != 0) 1 else 0
+            val rightWheelsLeft = if (wheel[w + 1] and (1 shl LEFT) != 0) 1 else 0
             if (wheelNowRight != rightWheelsLeft && !visit[w + 1]) {
                 visit[w + 1] = true
                 turn.add(Pair(w + 1, turnD * -1))
@@ -50,7 +52,7 @@ class `acw톱니바퀴` {
                 //반시계
                 val last = wheel[w] and (1 shl 7)
 
-                wheel[w] = wheel[w] shl 1
+                wheel[w] = (wheel[w] shl 1) and 255
                 if (last != 0) {
                     wheel[w]++
                 }
@@ -80,7 +82,7 @@ class `acw톱니바퀴` {
             for (i in input) {
                 num += (i.digitToInt() * m)
                 m /= 2
-            }
+            }//숫자로 바꿔서 저장
             wheel[it] = num
 
         }
@@ -88,23 +90,19 @@ class `acw톱니바퀴` {
         val turnCnt = readln().toInt()
         repeat(turnCnt) {
             val (w, d) = readln().split(" ").map { it.toInt() }
-            turn.add(Pair(w, d))
-            visit[w] = true
+            turn.add(Pair(w - 1, d))
+            visit[w - 1] = true
 
 
-            addTurn(w, d)
-            println(turn)
-            turnWheel()
-            for (i in wheel) {
-                println(i)
-            }
-            visit = Array(4) { false }
+            addTurn(w - 1, d)//회전해야할 것들 turn List에 추가해주기
+            turnWheel()// turn List에서 빼서 회전시켜주기
+            visit = Array(4) { false } // visit은 다시 사용해야함으로 다시 false로 초기화
         }
 
 
         var answer = 0
         for (i in 0 until 4) {
-            if ((wheel[i] and (1 shr 7)) == 0) {
+            if ((wheel[i] and (1 shl 7)) != 0) {
                 answer += (2.0).pow(i).toInt()
             }
         }
